403c49079b1a9745d8bb3ea5eeae0cfb3a922807
OakBricks <oakbricks@gmail.com>
2021-06-22 13:06:40 -0500

    trinkets is overrated

--- src/main/java/org/oakbricks/diseasemod/DiseaseMod.java
+++ src/main/java/org/oakbricks/diseasemod/DiseaseMod.java
@@ -1,18 +1,13 @@
 package org.oakbricks.diseasemod;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.oakbricks.diseasemod.trinkets.MaskTrinket;
 
 public class DiseaseMod implements ModInitializer {
 
 	public static final String MOD_ID = "diseasemod";
 	public static final Logger LOGGER = LogManager.getLogger();
-	public static Item FACE_MASK_TRINKET;
 
 	@Override
 	public void onInitialize() {
@@ -21,12 +16,5 @@ public class DiseaseMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("[DiseaseMod] OakBricks' Disease Mod has initialized, Good Luck!");
-		FACE_MASK_TRINKET = Registry.register(Registry.ITEM, identifier("face_mask"), new MaskTrinket(new Item.Settings().maxCount(1).maxDamage(100)));
-
 	}
-
-	private static Identifier identifier(String id) {
-		return new Identifier(MOD_ID, id);
-	}
-
 }
